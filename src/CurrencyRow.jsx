9fac50791dc48f6bdd303b87b304eee47d5741b4
Bumsoo Kim <kbeom00@gmail.com>
2023-09-22 22:04:00 +0900

    done css

---
+++ src/CurrencyRow.jsx
@@ -0,0 +1,11 @@
+const CurrencyRow = () => {
+  return (
+    <div>
+      <input type="number" className="input" />
+      <select name="currency-select" id="">
+        <option value="hi">hi</option>
+      </select>
+    </div>
+  );
+};
+export default CurrencyRow;
